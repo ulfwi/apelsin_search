faf35d9bde6afceb545461827bd61bf58371e1ba
Björn <bjorn.ulfwi@gmail.com>
2021-06-07 22:01:49 +0200

    Make some more funcitons

--- frontend.py
+++ frontend.py
@@ -1,7 +1,14 @@
 import curses
 from history import Searcher
 
+
 # https://docs.python.org/3/howto/curses.html
+
+class Position:
+    def __init__(self, x, y):
+        """ Create a new point at the origin """
+        self.x = x
+        self.y = y
 
 class GUI:
     def __init__(self, filepath):
@@ -13,20 +20,35 @@ class GUI:
         # curses.cbreak()
         self.stdscr.keypad(True)
 
+    def _get_cursor_pos(self):
+        y, x = self.stdscr.getyx()
+        return Position(x, y)
+
+    def _goto_pos(self, pos):
+        self.stdscr.move(pos.y, pos.x)
+
+    def _write(self, string):
+        self.stdscr.addstr(' ')
+
+    def _remove_last_char(self, search_phrase):
+        if search_phrase:
+            # Remove last letter
+            pos = self._get_cursor_pos()
+            new_pos = Position(pos.x-1, pos.y)
+            self._goto_pos(new_pos)
+            self._write(' ')
+            self._goto_pos(new_pos)
+            search_phrase = search_phrase[:-1]
+
+        return search_phrase
+
     def run(self):
         self.stdscr.addstr("$ ")
         search_phrase = ""
         while True:
             key = self.stdscr.getkey()
-            y_search_bar_cursor, x_search_bar_cursor = self.stdscr.getyx()
             if key == 'KEY_BACKSPACE':
-                if search_phrase:
-                    # Remove last letter
-                    self.stdscr.move(y_search_bar_cursor, x_search_bar_cursor-1)
-                    y_search_bar_cursor, x_search_bar_cursor = self.stdscr.getyx()
-                    self.stdscr.addstr(' ')
-                    self.stdscr.move(y_search_bar_cursor, x_search_bar_cursor)
-                    search_phrase = search_phrase[:-1]
+                search_phrase = self._remove_last_char(search_phrase)
             elif key == '\n':
                 break
             elif key in ['KEY_RIGHT', 'KEY_LEFT', 'KEY_UP', 'KEY_DOWN']:
@@ -77,7 +99,7 @@ if __name__ == '__main__':
     gui = GUI(filepath)
     output = gui.run()
 
-    del gui
+    # del gui
 
     print(output)
 
