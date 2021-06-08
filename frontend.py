01584d58e648e75c38bce98184a968b2505b6c7d
Björn <bjorn.ulfwi@gmail.com>
2021-06-08 09:51:00 +0200

    Separate backend from frontend

--- frontend.py
+++ frontend.py
@@ -10,15 +10,16 @@ class Position:
         self.x = x
         self.y = y
 
-class GUI:
-    def __init__(self, filepath):
-        self.filepath = filepath
-        self.searcher = Searcher(self.filepath)
 
+class GUI:
+    def __init__(self):
         self.stdscr = curses.initscr()
         curses.noecho()
         # curses.cbreak()
         self.stdscr.keypad(True)
+
+    def get_key(self):
+        return self.stdscr.getkey()
 
     def _get_cursor_pos(self):
         y, x = self.stdscr.getyx()
@@ -34,47 +35,61 @@ class GUI:
     def _write(self, string):
         self.stdscr.addstr(string)
 
-    def _remove_last_char(self, search_phrase):
-        if search_phrase:
-            # Remove last letter
-            pos = self._get_cursor_pos()
-            new_pos = Position(pos.x-1, pos.y)
-            self._goto_pos(new_pos)
-            self._write(' ')
-            self._goto_pos(new_pos)
-            search_phrase = search_phrase[:-1]
+    def _remove_last_char(self):
+        # Remove last letter
+        pos = self._get_cursor_pos()
+        new_pos = Position(pos.x-1, pos.y)
+        self._goto_pos(new_pos)
+        self._write(' ')
+        self._goto_pos(new_pos)
 
-        return search_phrase
+    def clear_remainder_of_screen(self):
+        self.stdscr.clrtobot()
+
+    def __del__(self):
+        # curses.nocbreak()
+        self.stdscr.keypad(False)
+        curses.echo()
+        curses.endwin()
+
+
+class HistorySearch:
+    def __init__(self, filepath):
+        self.filepath = filepath
+        self.searcher = Searcher(self.filepath)
+        self.gui = GUI()
 
     def run(self):
-        self._write("$ ")
+        self.gui._write("$ ")
         search_phrase = ""
         while True:
-            key = self.stdscr.getkey()
+            key = self.gui.get_key()
             if key == 'KEY_BACKSPACE':
-                search_phrase = self._remove_last_char(search_phrase)
+                if search_phrase:
+                    self.gui._remove_last_char()
+                    search_phrase = search_phrase[:-1]
             elif key == '\n':
                 break
             elif key in ['KEY_RIGHT', 'KEY_LEFT', 'KEY_UP', 'KEY_DOWN']:
                 pass
             else:
                 search_phrase += key
-                self._write(key)
+                self.gui._write(key)
 
-            pos_search_bar_cursor = self._get_cursor_pos()
+            pos_search_bar_cursor = self.gui._get_cursor_pos()
 
             search_phrase_list = search_phrase.split(' ')
             hits = self.searcher.search_for_phrases(search_phrase_list)
-            pos_max = self._get_max_pos()
+            pos_max = self.gui._get_max_pos()
 
             nbr_search_results = max(pos_max.y - 5, 5)
             pos_search_results = Position(0, pos_search_bar_cursor.y + 1)
 
             # Clear old results
-            self.stdscr.clrtobot()
+            self.gui.clear_remainder_of_screen()
 
             # Print top results
-            self._goto_pos(pos_search_results)
+            self.gui._goto_pos(pos_search_results)
             if hits:
                 for i in range(min(nbr_search_results, len(hits) - 1)):
                     try:
@@ -82,29 +97,22 @@ class GUI:
                         if len(command_str) >= pos_max.x-1:
                             # Don't print entire command if it's too long
                             command_str = command_str[:pos_max.x-1]
-                        self._write(command_str + '\n')
+                        self.gui._write(command_str + '\n')
                     except:
                         break
 
             # Move cursor back
-            self._goto_pos(pos_search_bar_cursor)
+            self.gui._goto_pos(pos_search_bar_cursor)
 
         return search_phrase
-
-    def __del__(self):
-        # Exit
-        # curses.nocbreak()
-        self.stdscr.keypad(False)
-        curses.echo()
-        curses.endwin()
 
 
 if __name__ == '__main__':
     filepath = '/home/s0001191/.bash_history'
-    gui = GUI(filepath)
-    output = gui.run()
+    history_search = HistorySearch(filepath)
+    output = history_search.run()
 
-    # del gui
+    del history_search
 
     print(output)
 
